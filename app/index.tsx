45cd221f3852663110e662ab1523cbe151123a4d
Carolina Moutella <emaildacarolmoutella@gmail.com>
2024-10-28 19:39:09 -0300

    reset app

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import "../constants/global.css";
+
+export default function Index() {
+  return (
+    <View className="flex justify-center px-1">
+      <Text>Bem vindo ao Blog do curso</Text>
+    </View>
+  );
+}

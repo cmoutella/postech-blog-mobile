f34b51165695012aadd8118accf2f537309fd617
Carolina Moutella <emaildacarolmoutella@gmail.com>
2024-10-29 12:04:33 -0300

    chore: set custom theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,11 @@
 import { Routes } from "@/routes";
 import { NativeBaseProvider } from "native-base";
 import "../constants/global.css";
+import { customTheme } from "@/constants/theme";
 
 export default function RootLayout() {
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={customTheme}>
       <Routes />
     </NativeBaseProvider>
   );
